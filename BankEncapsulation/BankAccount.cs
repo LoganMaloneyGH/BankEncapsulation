31179c55d4db099162bccf430bb5adbf7d5a88a4
logan Maloney <shakenasty13@gmail.com>
2021-11-09 10:03:11 -0600

    initial commit

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        public BankAccount()
+        { 
+        }
+        private double _balance = 0; 
+
+        public void Deposit(double depositAmount)
+        {
+            _balance += depositAmount;
+            Console.WriteLine($"deposit of {depositAmount.ToString("C")} has been accepted.\n");
+            Console.WriteLine($"Current Balance: {GetBalance().ToString("c")}\n");
+        }
+        public double GetBalance()
+        {
+            return _balance; 
+        }
+    }
+
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount logansAccount = new BankAccount();
+
+            Console.WriteLine($"Current Balance: {logansAccount.GetBalance()}\n");
+            Console.WriteLine("How much would you like to deposit?\n");
+            Console.WriteLine("$");
+            
+            var amountToDeposit = Convert.ToDouble(Console.ReadLine());
+           
+            logansAccount.Deposit(amountToDeposit); 
+        }
+    }
+}
